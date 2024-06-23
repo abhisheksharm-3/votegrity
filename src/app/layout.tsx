448bd023f8e347eeff14c824f54fd251457f6891
Abhishek Sharma <abhitiku2003@gmail.com>
2024-06-23 17:37:09 +0530

    minor tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
+    <html lang="en" className="overflow-x-hidden scrollbar-hide">
       <body className={inter.className}><Providers>{children}</Providers></body>
     </html>
   );
