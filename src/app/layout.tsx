45990364e09997d4baafea6493a806e1ed93a60d
Abhishek Sharma <abhitiku2003@gmail.com>
2024-06-26 19:52:03 +0530

    registeration page is now responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Votegrity",
-  description: "Blockchain Based dApp Voting WebAPp",
+  description: "Blockchain Based dApp Voting WebApp",
 };
 
 export default function RootLayout({
