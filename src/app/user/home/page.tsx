1e812c6369e7b8fc13d44b8ac53fb2a412720b19
abhitiku2003 <abhitiku2003@gmail.com>
2024-08-09 12:37:10 +0000

    added user profile page

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -32,7 +32,6 @@ interface MockUserData {
 const fadeInUp: Variants = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0 },
-  transition: { duration: 0.6 }
 };
 
 export default function HomePage() {
